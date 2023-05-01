4984f9315f54a6668c6d067ce9feb97e1ce318fc
vsol <vsol.sil@gmail.com>
2023-04-30 22:05:35 -0300

    add auth to product routes

--- backend/src/router/products.js
+++ backend/src/router/products.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const controller = require("../controllers/products.js");
+const { auth } = require("../middlewares/auth.js");
 
 const router = Router();
 
 router.get("/", controller.getProducts);
 router.get("/:id", controller.getProductById);
-router.post("/", controller.saveProduct);
-router.put("/:id", controller.updateProduct);
-router.delete("/:id", controller.deleteProduct);
+router.post("/", auth, controller.saveProduct);
+router.put("/:id", auth, controller.updateProduct);
+router.delete("/:id", auth, controller.deleteProduct);
 
 module.exports = router;
